27600b76fe32897f1d8110e552a1e8d86eb44a31
KiminSon <kimin0331@naver.com>
2023-11-13 03:19:11 +0900

    feat(InputView): 방문 날짜 범위 예외 처리
    validateReadDate 함수에서 방문 날짜 범위가 1이상 31이하의 숫자가 아닌 경우 예외 처리

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -7,17 +7,29 @@ public class InputView {
     private static final String ERROR_ORDER_MASSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
     public int readDate() {
-        try {
-            String input = Console.readLine();
-            int visitDate = Integer.parseInt(input);
-            return visitDate;
-        }catch (NumberFormatException e){
-            throw new IllegalArgumentException(ERROR_DATE_MASSAGE);
-        }
+        String input = Console.readLine();
+        validateReadDate(input);
+        return Integer.parseInt(input);
     }
 
     public String readItem() {
         String input = Console.readLine();
+        validateReadItem(input);
         return input;
+    }
+
+    public void validateReadDate(String input) {
+        try {
+            int visitDate = Integer.parseInt(input);
+            if (visitDate < 1 || visitDate > 31) {
+                throw new IllegalArgumentException(ERROR_DATE_MASSAGE);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERROR_DATE_MASSAGE);
+        }
+    }
+
+    public void validateReadItem(String input) {
+
     }
 }
